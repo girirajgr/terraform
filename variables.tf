d951f035190489b43a8fb26117c213c5738dc785
Giriraj <girirajpoc@gmail.com>
2017-11-01 21:45:49 +0530

    My first terraform

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "security_group_id" { default = "sg-aa2f19c3"}
+
+variable "key_name" { default = "girisnewlab" }
+
+variable "public_key" { default = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1C1ZwLcLylWeNdWqzOd9IrMwfXHMt+G5D0aAvVB558GKUhC2jZBLA2yM/zIvT7MkGp6wb8urYGU9QJmB/2DKK99wIthh+A3Ahnwnnik//ygX7zyz39zn5LniFPmoULMwb2lYhjje/vLM6us1A6nt5ZcE+/1h8R2mNUmEfAqO3tydxI0MHn/RWN+0O5kyNAWX0XoMvB578wfSnkgVl3Btiba4//Y68boAgNAEFQVgKzunr0xR54XzO9crXJZ4Qm6BGlQ2wLL6SkiPji91sKXeuAT2RPcwCETmnhEvpYinB7/tJc/M6Yzrk9OJfw9BE9oEktIbT3dj+AjpzpgTDkzepQIDAQAB" }
